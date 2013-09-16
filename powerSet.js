ff557cfd7de15319f14dbc6e935d16614d152afb
Ray <ray.yi@move.com>
2013-09-16 16:11:28 -0700

    init
    initial create project, with first test.

---
+++ powerSet.js
@@ -0,0 +1,63 @@
+/**
+ * Created with JetBrains WebStorm.
+ * User: ryi
+ * Date: 9/16/13
+ * Time: 9:33 AM
+ * To change this template use File | Settings | File Templates.
+ */
+
+
+function powerSet(sourceSet){
+    var n ,  item, j,
+        res = [];
+
+    if( sourceSet.length == 0){
+        res.push('');
+        return res;
+    }
+
+    item = sourceSet.pop();
+    res = powerSet(sourceSet).slice(0);
+    n = res.length;
+    for(j = 0; j<n; j++){
+        if(res[j]==''){
+            res.push(item+'');
+        }
+        else{
+//            res.push(item +',' + res[j]);
+            res.push(res[j] + ',' + item );
+        }
+    }
+
+    return res;
+}
+
+
+function test(input){
+    var testSet = input.split('');
+
+//    testSet.sort();
+
+    //remove duplicate
+    testSet = testSet.filter(function(item, index){
+        return testSet.indexOf(item) == index;
+    });
+
+    return powerSet(testSet);
+}
+
+function main(){
+    process.stdin.resume();
+    process.stdin.setEncoding('utf8');
+    process.stdout.write("please input a string: ");
+
+    process.stdin.on('data', function (data) {
+        data = data.toString().trim();
+        var res = test(data);
+//        res.sort();
+
+        console.dir(res);
+    });
+}
+
+main();
